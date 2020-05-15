f9c9dd265203cf78afbe43396334a979fc8a175d
kuuga <1596570410@qq.com>
2020-05-15 21:37:31 +0800

    首次存入全部文件

---
+++ js/nav2.js
@@ -0,0 +1,128 @@
+ //第一个下拉菜单
+ {$("#hf").mousemove(function(){   
+    $("#nav3-1").css({
+    "display":"block", 
+    "width": "1396px",
+    "height": "300px",
+    "background-color": "white",
+    "display": "flex",
+    "justify-content":"space-around",
+    "position": "absolute",
+    
+    "z-index": "888"
+    })
+    
+})
+
+$("#nav3-1").mousemove(function(){   
+    $("#nav3-1").css({
+        "display":"block", 
+    "width": "1396px",
+    "height": "300px",
+    "background-color": "white",
+    "display": "flex",
+    "justify-content":"space-around",
+    "position": "absolute",
+    
+    "z-index": "888"
+})
+})
+
+$("#hf").mouseout(function(){ 
+    $("#nav3-1").css({
+        "display":"none"
+})       
+})
+$("#nav3-1").mouseout(function(){
+    $("#nav3-1").css({
+        "display":"none"
+    })
+ })}
+
+
+
+ //第二个下拉菜单
+ { $("#cz").mousemove(function(){   
+    $("#nav3-2").css({
+    "display":"block", 
+    "width": "1396px",
+    "height": "300px",
+    "background-color": "white",
+    "display": "flex",
+    "justify-content":"space-around",
+    "position": "absolute",
+    
+    "z-index": "888"
+    })
+    
+})
+
+$("#nav3-2").mousemove(function(){   
+    $("#nav3-2").css({
+        "display":"block", 
+    "width": "1396px",
+    "height": "300px",
+    "background-color": "white",
+    "display": "flex",
+    "justify-content":"space-around",
+    "position": "absolute",
+    
+    "z-index": "888"
+})
+})
+
+$("#cz").mouseout(function(){ 
+    $("#nav3-2").css({
+        "display":"none"
+})       
+})
+$("#nav3-2").mouseout(function(){
+    $("#nav3-2").css({
+        "display":"none"
+    })
+ })}
+
+
+
+ //第三个下拉菜单
+ {$("#xs").mousemove(function(){   
+    $("#nav3-3").css({
+    "display":"block", 
+    "width": "1396px",
+    "height": "300px",
+    "background-color": "white",
+    "display": "flex",
+    "justify-content":"space-around",
+    "position": "absolute",
+    
+    "z-index": "888"
+    })
+    
+})
+
+$("#nav3-3").mousemove(function(){   
+    $("#nav3-3").css({
+        "display":"block", 
+    "width": "1396px",
+    "height": "300px",
+    "background-color": "white",
+    "display": "flex",
+    "justify-content":"space-around",
+    "position": "absolute",
+    
+    "z-index": "888"
+})
+})
+
+$("#xs").mouseout(function(){ 
+    $("#nav3-3").css({
+        "display":"none"
+})       
+})
+$("#nav3-3").mouseout(function(){
+    $("#nav3-3").css({
+        "display":"none"
+    })
+ })}
+ 
+
